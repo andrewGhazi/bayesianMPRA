ed752258b3301d3c2784d7eed8f95cdcd5b1b4d7
Andrew Ghazi <andghazi@gmail.com>
2017-10-13 14:27:15 -0500

    re-ran with corrected model object and individually written outputs

--- analysis/isolated_sampler_run.R
+++ analysis/isolated_sampler_run.R
@@ -5,65 +5,61 @@ library(parallel)
 library(fitdistrplus)
 library(preprocessCore)
 library(coda)
-source('~/bayesianMPRA/R/mledistModified.R')
+source('~/bayesianMPRA/analysis/mledistModified.R')
 
 load('~/bayesianMPRA/outputs/objects_for_isolated_run.RData')
 
-
-# sampler_results = varInfo %>% 
-#   group_by(construct) %>% 
-#   nest %>% 
-#   mutate(sampler_result = mclapply(data, run_sampler, mc.cores = 20))
-# 
-# save(sampler_results,
-#      file = '~/bayesianMPRA/outputs/sampler_results.RData')
-
-constructResults = sampler_results$sampler_result
-
-varInfo = sampler_results %>% 
-  dplyr::select(-sampler_result) %>% 
-  unnest %>% 
-  mutate(sampler_result = constructResults)
+sampler_results = varInfo %>%
+  group_by(construct) %>%
+  nest %>%
+  mutate(data = map2(data, construct, ~mutate(.x, construct = gsub('/', '-', gsub(' ', '_', .y))))) %$% 
+  mclapply(data, run_sampler, mc.cores = 9)
 
 quant_norm_parameter = function(param_name, sample_df){
-  sample_df %>% 
-    dplyr::select(contains(param_name)) %>% 
-    as.matrix %>% 
-    normalize.quantiles %>% 
+  sample_df %>%
+    dplyr::select(contains(param_name)) %>%
+    as.matrix %>%
+    normalize.quantiles %>%
     rowMeans %>% # Still not sure that taking the mean across samples is the right thing to do
-    as.tibble %>% 
+    as.tibble %>%
     set_names(param_name)
 }
 
 get_snp_TS_samples = function(sampler_res){
-  sample_df = sampler_res %>% 
+  sample_df = sampler_res %>%
     rstan::extract() %>%
-    map(as.tibble) %>% 
-    map2(names(.), 
-         ., 
+    map(as.tibble) %>%
+    map2(names(.),
+         .,
          ~set_names(.y, paste0(.x, '_', names(.y)))) %>%
     bind_cols
-  
+
   c('muMutRNA',
     'muMutDNA',
     'muRefRNA',
-    'muRefDNA') %>% 
-    map(quant_norm_parameter, sample_df = sample_df) %>% 
-    bind_cols %>% 
+    'muRefDNA') %>%
+    map(quant_norm_parameter, sample_df = sample_df) %>%
+    bind_cols %>%
     transmute(transcriptional_shift = log(muMutRNA / muMutDNA) - log(muRefRNA / muRefDNA))
 }
 
-varInfo %<>% 
-  mutate(transcriptional_shift_samples = map(sampler_result, get_snp_TS_samples),
+load_construct_get_TS = function(construct){
+  load(paste0('~/bayesianMPRA/outputs/ulirsch_sampler_results/', gsub('/', '-', gsub(' ', '_', construct)), '.RData'))
+  
+  get_snp_TS_samples(sampling_result)
+}
+
+varInfo %<>%
+  mutate(transcriptional_shift_samples = map(construct, load_construct_get_TS),
          transcriptional_shift_HDI = map(transcriptional_shift_samples, ~pull(.x, transcriptional_shift) %>% mcmc %>% HPDinterval(prob = .99)),
          functional = map_lgl(transcriptional_shift_HDI, ~!between(0, .x[1], .x[2])),
-         mean_transcriptional_shift = map_dbl(transcriptional_shift_samples, ~pull(.x, transcriptional_shift) %>% mean)) 
+         mean_transcriptional_shift = map_dbl(transcriptional_shift_samples, ~pull(.x, transcriptional_shift) %>% mean))
 
 save(varInfo, file = '~/bayesianMPRA/outputs/varInfo_with_samples.RData')
 
-bayesian_functional = varInfo %>% 
-  filter(functional) %>% 
-  arrange(desc(abs(mean_transcriptional_shift))) %>% 
+bayesian_functional = varInfo %>%
+  filter(functional) %>%
+  arrange(desc(abs(mean_transcriptional_shift))) %>%
   filter(abs(mean_transcriptional_shift) < 10)
 
 save(bayesian_functional, file = '~/bayesianMPRA/outputs/functional_varInfo.RData')
