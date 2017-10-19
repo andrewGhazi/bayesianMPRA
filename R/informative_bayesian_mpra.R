3aed00d8e003097686d908dc5d21f774925e580a
ANDREW GHAZI <arghazi@bcm.edu>
2017-10-19 10:07:09 -0400

    unlisting result

--- R/informative_bayesian_mpra.R
+++ R/informative_bayesian_mpra.R
@@ -671,12 +671,12 @@ bayesian_mpra_analyze = function(mpra_data,
   mpra_data %>% 
     group_by(snp_id) %>%
     nest %>%
-    mutate(sampler_result = mclapply(data, 
+    mutate(sampler_result = unlist(mclapply(data, 
                                      run_sampler, 
                                      marg_dna_prior = marg_dna_prior, 
                                      save_nonfunctional = save_nonfunctional,
                                      out_dir = out_dir,
                                      norm_method = normalization_method,
-                                     mc.cores = num_cores))
+                                     mc.cores = num_cores)))
   
 }
