ca8be2d3f04ed40d11dbedbe68968667ab796d5c
ANDREW GHAZI <arghazi@bcm.edu>
2017-11-02 12:07:20 -0500

    using new nb fitter

--- analysis/dist_fitting_redo.R
+++ analysis/dist_fitting_redo.R
@@ -13,7 +13,7 @@ mpra_data$count_data[[9]] %>%
   summarise(nb_fit = list(fitdist(count, distr = 'nbinom')))
 
 
-est_sample_params = function(count_dat){
+est_sample_nb = function(count_dat){
   # count_dat - a tibble with a allele (ref/mut) column and columns of observed MPRA counts in given transfections
   # uses fitdistrplus::fitdist because MASS::fitdistr was cracking wise at me
   # using a modified version of fitdistrplus::mledist because the regular version can't use non-integer weights (and it was cracking wise)
@@ -21,17 +21,12 @@ est_sample_params = function(count_dat){
   count_dat %>% 
     gather(block, count, -allele) %>% 
     group_by(allele, block) %>% 
-    summarise(mle_neg_bin = list(safelyFitNegBin(count))) %>% 
+    summarise(mle_neg_bin = list(fit_nb(count))) %>% 
     ungroup %>% 
-    mutate(nb_mu_est = map_dbl(mle_neg_bin, ~.x$result$estimate['mu']),
-           nb_size_est = map_dbl(mle_neg_bin, ~.x$result$estimate['size'])) %>% 
+    filter(map_lgl(mle_neg_bin, ~class(.x) == 'fitdist')) %>% 
+    mutate(nb_mu_est = map_dbl(mle_neg_bin, ~.x$estimate['mu']),
+           nb_size_est = map_dbl(mle_neg_bin, ~.x$estimate['size'])) %>% 
     dplyr::select(-mle_neg_bin)
 }
 
-safelyFitNegBin = function(count_vec){
-  #This doesn't quite suppress all error messages but it does output the right things
-  #an error can still be printed when there's very low variability of low counts (e.g. c(1, rep(0, 14)))
-  safely(fitdist, 
-         otherwise = NA, 
-         quiet = TRUE)(count_vec, 'nbinom')
-}
+
