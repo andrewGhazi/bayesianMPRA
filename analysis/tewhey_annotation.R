cf89cf54a02b2f5421b262ed4ca262ba6be3a551
Andrew Ghazi <andghazi@gmail.com>
2018-03-14 14:30:25 -0500

    importing the correct data, dna depth normalization

--- analysis/tewhey_annotation.R
+++ analysis/tewhey_annotation.R
@@ -1,26 +1,62 @@
 ## Let's annotate the Tewhey data
 
 library(parallel)
+library(data.table)
+library(magrittr)
 library(tidyverse)
 
+sample_sums = read_tsv('/mnt/bigData2/andrew/MPRA/Tewhey/GSE75661_79k_collapsed_counts.txt', # this data is already summed across barcodes, I'm just using it to get the sample depths
+                      col_names = TRUE) %>%
+  separate(Oligo, into = c('snp_id', 'allele'), sep = stringr::regex('(?=[AB]$)')) %>%
+  mutate(snp_id = gsub(pattern = '_$', replacement = '', x = snp_id, perl = TRUE)) %>%
+  gather(sample, count, -snp_id, -allele)
+
+tewhey_depths = sample_sums %>%
+  group_by(sample) %>%
+  summarise(depth = sum(count))
+
+dna_depths = tewhey_depths %>% 
+  filter(grepl('Plasmid', sample)) %>% 
+  mutate(sample = str_extract(sample, 'r[0-9]')) %>% 
+  rename(file = sample) %>% 
+  as.data.table %>% 
+  .[,file := factor(file)] %>% 
+  setkey(file)
 
 dir = '/mnt/bigData2/andrew/MPRA/Tewhey/indivTags/'
+dna_data_files = list.files(dir,
+                            pattern = 'ctrl')
 
-file_names = list.files(dir, 
-                        pattern = '.expanded$') %>% 
-  grep(pattern = 'ctrl|HepG2', x = ., value = TRUE)
+dna_counts = map(dna_data_files, ~fread(paste0(dir, .x),
+                                        col.names = c('snp_id', 'barcode', 'count'))[,file := str_extract(.x, 'r[0-9]')]) %>% 
+  bind_rows %>% 
+  .[,file := factor(file)] %>% 
+  setkey(file)
 
-tewhey_dat = mclapply(file_names,
-                         function(t_file){
-                           read_tsv(paste0(dir, t_file), 
-                                    col_names = c('snp_allele', 'barcode', 'count')) %>% 
-                             mutate(sample = t_file) %>% 
-                             separate(snp_allele, into = c('snp_id', 'allele'), sep = stringr::regex('(?=[AB]$)')) %>% 
-                             mutate(snp_id = gsub('_$', '', snp_id))
-                         },
-                         mc.cores = 11) %>% 
-  bind_rows
+dna_norm_depths = dna_counts[dna_depths] %>% 
+  .[,norm_depth := count * 1e6 / depth] %>% 
+  .[,.(snp_id = snp_id[1], mean_norm_depth = mean(norm_depth)), by = barcode]
 
-save(tewhey_subset, file = '~/bayesianMPRA/analysis_data/tewhey_subset.RData')
+dna_norm_depths %>% ggplot(aes(mean_norm_depth)) + geom_histogram(bins = 50) + scale_x_log10() + geom_vline(xintercept = .03, lty = 2)
 
-
+#### This data is already summed across barcodes
+# tewhey_dat = read_tsv('/mnt/bigData2/andrew/MPRA/Tewhey/GSE75661_79k_collapsed_counts.txt',
+#                       col_names = TRUE) %>%
+#   separate(Oligo, into = c('snp_id', 'allele'), sep = stringr::regex('(?=[AB]$)')) %>%
+#   mutate(snp_id = gsub(pattern = '_$', replacement = '', x = snp_id, perl = TRUE)) %>%
+#   gather(sample, count, -snp_id, -allele)
+# 
+# tewhey_depths = tewhey_dat %>%
+#   group_by(sample) %>%
+#   summarise(depth = sum(count))
+# 
+# tewhey_dna = tewhey_dat %>% 
+#   filter(grepl('Plasmid', sample))
+# 
+# tehey_dna %>% 
+#   filter(grepl('Plasmid', sample)) %>% 
+#   ggplot(aes(count)) +
+#   geom_histogram(bins = 40) + 
+#   facet_wrap('sample') +
+#   scale_x_log10()
+# 
